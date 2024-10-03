d99bfe1d453a2ca88a990843cf30f82c960108b1
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2024-10-04 00:02:07 +0200

    ✨ [GH-1] provided dependency graph

---
+++ core/src/main/kotlin/io/mzlnk/javalin/boot/core/internal/graph/DependencyGraph.kt
@@ -0,0 +1,83 @@
+package io.mzlnk.javalin.boot.core.internal.graph
+
+import io.mzlnk.javalin.boot.core.internal.definition.SingletonDefinition
+
+internal class DependencyGraph private constructor(
+    private val _nodes: Array<SingletonDefinition>,
+    private val _edges: Array<Array<Boolean>>
+) {
+
+    val nodes: List<SingletonDefinition> = _nodes.toList()
+    val edges: List<Pair<SingletonDefinition, SingletonDefinition>> = _edges
+        .mapIndexed { i, row -> row.mapIndexedNotNull { j, edge -> if(edge) i to j else null } }
+        .flatten()
+        .map { (i, j) -> _nodes[i] to _nodes[j] }
+
+    companion object {
+
+        fun create(definitions: List<SingletonDefinition>): DependencyGraph {
+            val nodes: Array<SingletonDefinition> = definitions.toTypedArray()
+            val nodesIdxsByKey: Map<SingletonDefinition.Key, List<Int>> = nodes
+                .mapIndexed { idx, def -> def.key to idx }
+                .groupBy({ it.first }, { it.second })
+
+            val edges: Array<Array<Boolean>> = Array(nodes.size) { Array(nodes.size) { false } }
+
+            nodes.forEachIndexed { idx, node ->
+                val dependentNodesIdxs: List<Int> = node.dependencies
+                    .flatMap { dep ->
+                        nodesIdxsByKey
+                            /*
+                                node \ dep | no-name | name                   |
+                                no-name    |    1    |  0                     |
+                                name       |    1    |  node.name == dep.name |
+                             */
+                            .filterKeys { key -> key.type == dep.type && dep.name?.let { name -> name == key.name } ?: true }
+                            .values
+                            .flatten()
+                            .also {
+                                if(dep is SingletonDefinition.Dependency.Single) {
+                                    if(it.isEmpty()) {
+                                        throw DependencyGraphCreationException.DependencyNotFound()
+                                    }
+                                    if(it.size > 1) {
+                                        throw DependencyGraphCreationException.MultipleDependenciesFound()
+                                    }
+                                }
+                            }
+                    }
+
+                dependentNodesIdxs.forEach { dependentNodeIdx ->
+                    edges[dependentNodeIdx][idx] = true
+                }
+            }
+
+            // validate against circular dependencies
+            for(i in nodes.indices) {
+                for(j in nodes.indices) {
+                    if(i == j) {
+                        continue
+                    }
+
+                    if(edges[i][j] && edges[j][i]) {
+                        throw DependencyGraphCreationException.CircularDependencyFound()
+                    }
+                }
+            }
+
+            return DependencyGraph(
+                _nodes = nodes,
+                _edges = edges
+            )
+        }
+    }
+
+}
+
+internal abstract class DependencyGraphCreationException(message: String) : RuntimeException(message) {
+
+    class DependencyNotFound : DependencyGraphCreationException("Dependency not found")
+    class MultipleDependenciesFound : DependencyGraphCreationException("Multiple dependencies found")
+    class CircularDependencyFound : DependencyGraphCreationException("Circular dependency found")
+
+}
---
+++ core/src/test/kotlin/io/mzlnk/javalin/boot/core/internal/graph/DependencyGraphTest.kt
@@ -0,0 +1,536 @@
+package io.mzlnk.javalin.boot.core.internal.graph
+
+import io.mzlnk.javalin.boot.core.internal.definition.singletonDefinition
+import io.mzlnk.javalin.boot.core.internal.utils.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DependencyGraphTest {
+
+    @Test
+    fun `should create graph for dependent singletons`() {
+        /*
+         * dependency graph:
+         * A <- B
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                single { type = B }
+            }
+        }
+        val singletonB = singletonDefinition(B)
+
+        // and:
+        val definitions = listOf(singletonA, singletonB)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactly(singletonB to singletonA)
+    }
+
+    @Test
+    fun `should create graph for chain of dependent singletons`() {
+        /*
+         * dependency graph:
+         * A <- B <- C
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                single { type = B }
+            }
+        }
+        val singletonB = singletonDefinition(B) {
+            dependencies {
+                single { type = C }
+            }
+        }
+        val singletonC = singletonDefinition(C)
+
+        // and:
+        val definitions = listOf(singletonA, singletonB, singletonC)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactlyInAnyOrder(
+            singletonC to singletonB,
+            singletonB to singletonA
+        )
+    }
+
+    @Test
+    fun `should create graph for multiple dependency trees`() {
+        /*
+         * dependency graph:
+         * A <- B
+         * C <- D
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                single { type = B }
+            }
+        }
+
+        val singletonB = singletonDefinition(B)
+
+        val singletonC = singletonDefinition(C) {
+            dependencies {
+                single { type = D }
+            }
+        }
+
+        val singletonD = singletonDefinition(D)
+
+        // and:
+        val definitions = listOf(singletonA, singletonB, singletonC, singletonD)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactlyInAnyOrder(
+            singletonB to singletonA,
+            singletonD to singletonC
+        )
+    }
+
+    @Test
+    fun `should create graph for multiple dependencies for one singleton`() {
+        /*
+         * dependency graph:
+         * C -> A <- B
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                single { type = B }
+                single { type = C }
+            }
+        }
+
+        val singletonB = singletonDefinition(B)
+        val singletonC = singletonDefinition(C)
+
+        // and:
+        val definitions = listOf(singletonA, singletonB, singletonC)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactlyInAnyOrder(
+            singletonB to singletonA,
+            singletonC to singletonA
+        )
+    }
+
+    @Test
+    fun `should create graph for multiple dependencies for multiple singletons in chain`() {
+        /*
+         * dependency graph:
+         * C -> A
+         *      ^
+         *      |
+         * D -> B <- E
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                single { type = C }
+                single { type = B }
+            }
+        }
+
+        val singletonB = singletonDefinition(B) {
+            dependencies {
+                single { type = D }
+                single { type = E }
+            }
+        }
+
+        val singletonC = singletonDefinition(C)
+        val singletonD = singletonDefinition(D)
+        val singletonE = singletonDefinition(E)
+
+        // and:
+        val definitions = listOf(singletonA, singletonB, singletonC, singletonD, singletonE)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactlyInAnyOrder(
+            singletonC to singletonA,
+            singletonB to singletonA,
+            singletonD to singletonB,
+            singletonE to singletonB
+        )
+    }
+
+    @Test
+    fun `should create graph for singleton dependent to multiple singletons`() {
+        /*
+         * dependency graph:
+         * A <- C -> B
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                single { type = C }
+            }
+        }
+
+        val singletonB = singletonDefinition(B) {
+            dependencies {
+                single { type = C }
+            }
+        }
+
+        val singletonC = singletonDefinition(C)
+
+        // and:
+        val definitions = listOf(singletonA, singletonB, singletonC)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactlyInAnyOrder(
+            singletonC to singletonA,
+            singletonC to singletonB
+        )
+    }
+
+    @Test
+    fun `should create graph for diamond shaped dependencies`() {
+        /*
+         * dependency graph:
+         *   -> B ->
+         * D         A
+         *   -> C ->
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                single { type = B }
+                single { type = C }
+            }
+        }
+
+        val singletonB = singletonDefinition(B) {
+            dependencies {
+                single { type = D }
+            }
+        }
+
+        val singletonC = singletonDefinition(C) {
+            dependencies {
+                single { type = D }
+            }
+        }
+
+        val singletonD = singletonDefinition(D)
+
+        // and:
+        val definitions = listOf(singletonA, singletonB, singletonC, singletonD)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactlyInAnyOrder(
+            singletonB to singletonA,
+            singletonC to singletonA,
+            singletonD to singletonB,
+            singletonD to singletonC
+        )
+    }
+
+    @Test
+    fun `should create graph for iterable dependency with one dependency`() {
+        /*
+         * dependency graph:
+         * A <- B
+         * dep(A) = List<B>
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                iterable {
+                    type = B
+                    iterableType = List::class.java
+                }
+            }
+        }
+        val singletonB1 = singletonDefinition(B)
+
+        // and:
+        val definitions = listOf(singletonA, singletonB1)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactly(singletonB1 to singletonA)
+    }
+
+    @Test
+    fun `should create graph for iterable dependency with no dependencies`() {
+        /*
+         * dependency graph:
+         * A
+         * dep(A) = List<B>
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                iterable {
+                    type = B
+                    iterableType = List::class.java
+                }
+            }
+        }
+
+        // and:
+        val definitions = listOf(singletonA)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).isEmpty()
+    }
+
+    @Test
+    fun `should create graph for iterable dependency with multiple dependencies`() {
+        /*
+         * dependency graph:
+         * A <- B
+         * dep(A) = List<B>
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                iterable {
+                    type = B
+                    iterableType = List::class.java
+                }
+            }
+        }
+        val singletonB1 = singletonDefinition(B)
+        val singletonB2 = singletonDefinition(B)
+        val singletonB3 = singletonDefinition(B)
+
+        // and:
+        val definitions = listOf(singletonA, singletonB1, singletonB2, singletonB3)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactlyInAnyOrder(
+            singletonB1 to singletonA,
+            singletonB2 to singletonA,
+            singletonB3 to singletonA
+        )
+    }
+
+    @Test
+    fun `should create graph for named dependency`() {
+        /*
+         * dependency graph:
+         * A <- B<b1>
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                single {
+                    type = B
+                    name = "b1"
+                }
+            }
+        }
+        val singletonB_b1 = singletonDefinition(B, name = "b1")
+
+        // and:
+        val definitions = listOf(singletonA, singletonB_b1)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactly(singletonB_b1 to singletonA)
+    }
+
+    @Test
+    fun `should create graph for multiple named dependencies`() {
+        /*
+         * dependency graph:
+         * A <- C<c1>
+         * B <- C<c2>
+         * C
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                single {
+                    type = C
+                    name = "c1"
+                }
+            }
+        }
+        val singletonB = singletonDefinition(B) {
+            dependencies {
+                single {
+                    type = C
+                    name = "c2"
+                }
+            }
+        }
+
+        val singletonC_c1 = singletonDefinition(C, name = "c1")
+        val singletonC_c2 = singletonDefinition(C, name = "c2")
+        val singletonC = singletonDefinition(C)
+
+        // and:
+        val definitions = listOf(singletonA, singletonB, singletonC_c1, singletonC_c2, singletonC)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactlyInAnyOrder(
+            singletonC_c1 to singletonA,
+            singletonC_c2 to singletonB
+        )
+    }
+
+    @Test
+    fun `should create graph for iterable dependency with one named dependency`() {
+        /*
+         * dependency graph:
+         * A <- B
+         * dep(A) = List<B>
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                iterable {
+                    type = B
+                    iterableType = List::class.java
+                }
+            }
+        }
+        val singletonB1_b1 = singletonDefinition(B, name = "b1")
+
+        // and:
+        val definitions = listOf(singletonA, singletonB1_b1)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactly(singletonB1_b1 to singletonA)
+    }
+
+    @Test
+    fun `should create graph for iterable named dependency with one named dependency`() {
+        /*
+         * dependency graph:
+         * A <- B<b1>
+         * dep(A) = List<B<b1>>
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                iterable {
+                    type = B
+                    iterableType = List::class.java
+                    name = "b1"
+                }
+            }
+        }
+        val singletonB1_b1 = singletonDefinition(B, name = "b1")
+
+        // and:
+        val definitions = listOf(singletonA, singletonB1_b1)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactly(singletonB1_b1 to singletonA)
+    }
+
+    @Test
+    fun `should create graph for iterable named dependency with multiple named dependencies`() {
+        /*
+         * dependency graph:
+         * A <- B<b1>
+         * dep(A) = List<B<b1>>
+         */
+
+        // given:
+        val singletonA = singletonDefinition(A) {
+            dependencies {
+                iterable {
+                    type = B
+                    iterableType = List::class.java
+                    name = "b1"
+                }
+            }
+        }
+        val singletonB1_b1 = singletonDefinition(B, name = "b1")
+        val singletonB2_b1 = singletonDefinition(B, name = "b1")
+        val singletonB2_b2 = singletonDefinition(B, name = "b2")
+        val singletonB3 = singletonDefinition(B)
+
+        // and:
+        val definitions = listOf(singletonA, singletonB1_b1, singletonB2_b1, singletonB2_b2, singletonB3)
+
+        // when:
+        val graph = DependencyGraph.create(definitions)
+
+        // then:
+        assertThat(graph.nodes).isEqualTo(definitions)
+        assertThat(graph.edges).containsExactlyInAnyOrder(
+            singletonB1_b1 to singletonA,
+            singletonB2_b1 to singletonA,
+        )
+    }
+
+}
