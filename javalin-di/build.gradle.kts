9e530142ab64564fb760d50ab07ee23746fb2371
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2024-11-10 21:18:35 +0100

    ⚗️ [GH-1] provided initial skeleton for e2e tests

--- javalin-di/build.gradle.kts
+++ javalin-di/build.gradle.kts
@@ -1,7 +1,10 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     kotlin("jvm") version "_"
     `maven-publish`
     jacoco
+    idea
 }
 
 group = "io.mzlnk"
@@ -12,7 +15,55 @@ repositories {
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
+}
+
+sourceSets {
+    create("e2eTest") {
+        kotlin {
+            srcDir("src/e2eTest/kotlin")
+        }
+        resources {
+            srcDir("src/e2eTest/resources")
+        }
+
+        compileClasspath += sourceSets["main"].output
+        runtimeClasspath += sourceSets["main"].output
+    }
+}
+
+idea {
+    module {
+        testSources.from(
+            sourceSets["e2eTest"].kotlin.srcDirs
+        )
+        testResources.from(
+            sourceSets["e2eTest"].resources.srcDirs
+        )
+    }
+}
+
+val e2eTestImplementation = configurations["e2eTestImplementation"]
+
+configurations.create("cucumberRuntime") {
+    extendsFrom(configurations["e2eTestImplementation"])
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.google.devtools.ksp:symbol-processing-api:_")
+    implementation("com.squareup:kotlinpoet:_")
+
+    compileOnly("io.javalin:javalin:_")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:_")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:_")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:_")
+
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:_")
+
+    testImplementation("org.assertj:assertj-core:_")
+    testImplementation(kotlin("reflect"))
 }
 
 publishing {
@@ -49,6 +100,11 @@ dependencies {
 
     testImplementation("org.assertj:assertj-core:_")
     testImplementation(kotlin("reflect"))
+
+    e2eTestImplementation("io.cucumber:cucumber-java:_")
+    e2eTestImplementation("io.cucumber:cucumber-core:_")
+    e2eTestImplementation("io.cucumber:cucumber-expressions:_")
+    e2eTestImplementation("org.assertj:assertj-core:_")
 }
 
 jacoco {
@@ -65,6 +121,29 @@ tasks.jacocoTestReport {
 
     reports {
         html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
+}
+
+tasks.register("e2eTest") {
+    dependsOn("build")
+    dependsOn("e2eTestClasses")
+
+    doLast {
+        javaexec {
+            mainClass = "io.cucumber.core.cli.Main"
+            classpath = configurations["cucumberRuntime"] + sourceSets["e2eTest"].runtimeClasspath
+        }
+    }
+}
+
+tasks.withType<Copy> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+        freeCompilerArgs.add("-Xjvm-default=all")
     }
 }
 
---
+++ javalin-di/src/e2eTest/resources/features/sample.feature
@@ -0,0 +1,6 @@
+Feature: Sample feature
+
+  Scenario: Sample scenario
+    Given sample given step
+    When sample when step
+    Then sample then step
