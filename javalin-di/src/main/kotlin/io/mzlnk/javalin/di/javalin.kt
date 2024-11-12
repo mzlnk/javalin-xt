e71eff9fc2af25fd00dcfd9b37d77ba56767d07c
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2024-11-12 22:44:54 +0100

    test3

--- javalin-di/src/main/kotlin/io/mzlnk/javalin/di/javalin.kt
+++ javalin-di/src/main/kotlin/io/mzlnk/javalin/di/javalin.kt
@@ -13,6 +13,7 @@ private val LOG = LoggerFactory.getLogger("io.mzlnk.javalin.di")
 
 // test-1
 // test-2
+// test-3
 
 fun Javalin.enableDI(): Javalin {
     val (context, elapsedTime) = measureTimedValue {
