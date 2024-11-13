51760da6f1b2a5a90366e2a5e3155fcb7b1a6b15
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2024-11-13 20:42:59 +0100

    ♻️ [GH-1] refactored definition provider file generator

--- javalin-di/src/main/kotlin/io/mzlnk/javalin/di/internal/processing/SingletonDefinitionProviderFileGenerator.kt
+++ javalin-di/src/main/kotlin/io/mzlnk/javalin/di/internal/processing/SingletonDefinitionProviderFileGenerator.kt
@@ -6,6 +6,7 @@ import io.mzlnk.javalin.di.definition.SingletonDefinition
 import io.mzlnk.javalin.di.definition.SingletonDefinitionProvider
 import io.mzlnk.javalin.di.type.TypeReference
 
+// TODO: refactor it
 internal object SingletonDefinitionProviderFileGenerator {
 
     fun generate(module: ModuleClass): GeneratedFile {
@@ -16,84 +17,8 @@ internal object SingletonDefinitionProviderFileGenerator {
             .addType(
                 TypeSpec.classBuilder("${module.type.name}SingletonDefinitionProvider")
                     .addSuperinterface(SingletonDefinitionProvider::class)
-                    .addProperty(
-                        PropertySpec.builder(
-                            name = "module",
-                            type = ClassName(module.type.packageName, module.type.name),
-                            modifiers = listOf(KModifier.PRIVATE),
-                        )
-                            .initializer("%T()", ClassName(module.type.packageName, module.type.name))
-                            .build()
-                    )
-                    .addProperty(
-                        PropertySpec.builder(
-                            name = "definitions",
-                            type = List::class.asClassName()
-                                .parameterizedBy(SingletonDefinition::class.asClassName().parameterizedBy(STAR)),
-                            modifiers = listOf(KModifier.OVERRIDE)
-                        )
-                            .initializer(
-                                CodeBlock.builder()
-                                    .add("listOf(\n")
-                                    .indent()
-                                    .apply {
-                                        module.singletons.forEachIndexed { idx, method ->
-                                            add("%T(\n", SingletonDefinition::class)
-                                            indent()
-                                            add("identifier = %T(\n", SingletonDefinition.Identifier::class)
-                                            indent()
-                                            add(
-                                                "typeRef = object : %T<%L>() {}\n",
-                                                TypeReference::class.java,
-                                                method.returnType.qualifiedName
-                                            )
-                                            unindent()
-                                            add("),\n")
-                                            add("dependencies = ")
-                                            method.parameters
-                                                .takeIf { it.isNotEmpty() }
-                                                ?.let {
-                                                    add("listOf(\n")
-                                                    indent()
-                                                    method.parameters.forEach { parameter ->
-                                                        add(
-                                                            "%T(typeRef = object : %T<%L>() {}),\n",
-                                                            SingletonDefinition.Identifier::class.java,
-                                                            TypeReference::class.java,
-                                                            parameter.type.qualifiedName
-                                                        )
-                                                    }
-                                                    unindent()
-                                                    add("),\n")
-                                                }
-                                                ?: add("emptyList(),\n")
-                                            add("instanceProvider = {\n")
-                                            indent()
-                                            add("module.%L(", method.name)
-                                            indent()
-                                            method.parameters.forEachIndexed { index, parameter ->
-                                                add(
-                                                    "\nit[%L] as %L",
-                                                    index,
-                                                    parameter.type.qualifiedName
-                                                )
-                                                if (index < method.parameters.size - 1) add(",")
-                                            }
-                                            unindent()
-                                            if(method.parameters.isNotEmpty()) add("\n")
-                                            add(")\n")
-                                            unindent()
-                                            add("}\n")
-                                            unindent()
-                                            if (idx < module.singletons.size - 1) add("),\n") else add(")\n")
-                                        }
-                                    }
-                                    .unindent()
-                                    .add(")")
-                                    .build()
-                            )
-                            .build()
-                    )
+                    .addProperty(moduleProperty(module))
+                    .addProperty(definitionsProperty(module))
                     .build()
             )
             .build()
@@ -106,4 +31,88 @@ internal object SingletonDefinitionProviderFileGenerator {
         )
     }
 
+    private fun moduleProperty(module: ModuleClass): PropertySpec =
+        PropertySpec.builder(
+            name = "module",
+            type = ClassName(module.type.packageName, module.type.name),
+            modifiers = listOf(KModifier.PRIVATE),
+        )
+            .initializer("%T()", ClassName(module.type.packageName, module.type.name))
+            .build()
+
+    private fun definitionsProperty(module: ModuleClass): PropertySpec =
+        PropertySpec.builder(
+            name = "definitions",
+            type = List::class
+                .asClassName()
+                .parameterizedBy(SingletonDefinition::class.asClassName().parameterizedBy(STAR)),
+            modifiers = listOf(KModifier.OVERRIDE)
+        )
+            .initializer(
+                CodeBlock.builder()
+                    .add("listOf(\n")
+                    .indent()
+                    .apply {
+                        module.singletons.forEachIndexed { idx, method ->
+                            addSingletonDefinitionInstance(method)
+                            if (idx < module.singletons.size - 1) add(",")
+                            add("\n")
+                        }
+                    }
+                    .unindent()
+                    .add(")")
+                    .build()
+            )
+            .build()
+
+    private fun CodeBlock.Builder.addSingletonDefinitionInstance(method: SingletonMethod) =
+        this
+            .add("%T(\n", SingletonDefinition::class)
+            .indent()
+            .add("identifier = %T(\n", SingletonDefinition.Identifier::class)
+            .indent()
+            .add("typeRef = object : %T<%L>() {}\n", TypeReference::class.java, method.returnType.qualifiedName)
+            .unindent()
+            .add("),\n")
+            .add("dependencies = ")
+            .apply {
+                method.parameters
+                    .takeIf { it.isNotEmpty() }
+                    ?.let {
+                        add("listOf(\n")
+                        indent()
+                        method.parameters.forEach { parameter ->
+                            add(
+                                "%T(typeRef = object : %T<%L>() {}),\n",
+                                SingletonDefinition.Identifier::class.java,
+                                TypeReference::class.java,
+                                parameter.type.qualifiedName
+                            )
+                        }
+                        unindent()
+                        add("),\n")
+                    }
+                    ?: add("emptyList(),\n")
+            }
+            .add("instanceProvider = {\n")
+            .indent()
+            .add("module.%L(", method.name)
+            .indent()
+            .apply {
+                method.parameters.forEachIndexed { index, parameter ->
+                    add(
+                        "\nit[%L] as %L",
+                        index,
+                        parameter.type.qualifiedName
+                    )
+                    if (index < method.parameters.size - 1) add(",")
+                }
+            }
+            .unindent()
+            .apply { if (method.parameters.isNotEmpty()) add("\n") }
+            .add(")\n")
+            .unindent()
+            .add("}\n")
+            .unindent()
+            .add(")")
 }
