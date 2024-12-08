dcbb97e38d6695c0ffe9527409b4c4919c8e3489
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2024-12-08 17:57:12 +0100

    🔖 [GH-0] release 0.2.0

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     kotlin("jvm") version "2.0.21"
-    id("io.mzlnk.javalin-xt") version "0.1.0"
+    id("io.mzlnk.javalin-xt") version "0.2.0"
 }
 
 group = "io.mzlnk"
-version = "0.0.1"
+version = "0.2.0"
 
 kotlin {
     jvmToolchain(21)
