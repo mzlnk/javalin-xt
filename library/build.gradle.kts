393385f75047eb194a96f713f0f4265a183d1619
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2025-01-23 20:26:01 +0100

    🔖 [GH-0] release 0.5.0

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "io.mzlnk"
-version = "0.5.0-SNAPSHOT"
+version = "0.5.0"
 
 kotlin {
     jvmToolchain(17)
@@ -98,7 +98,7 @@ publishing {
 
             groupId = "io.mzlnk"
             artifactId = "javalin-xt"
-            version = "0.5.0-SNAPSHOT"
+            version = "0.5.0"
 
             artifact(tasks.getByName("sourcesJar"))
             artifact(tasks.getByName("javadocJar"))
